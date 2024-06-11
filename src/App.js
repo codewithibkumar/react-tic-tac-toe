e270b14802e673ea10d219e88b67e12ea9bb0a9d
ibkumar <ibkumarandroid@gmail.com>
2024-06-12 02:46:32 +0530

    Wring winning condition done and checking score on mounting using useEffect but not working

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Cell from "./components/Cell";
 const App = () => {
 
@@ -7,6 +7,37 @@ const App = () => {
   const [winningMessage, setWinningMessage] = useState(null);
 
   const message = "It is now " + go + "'s go."
+
+  const checkScore = () => {
+    const winningCondition = [
+      [0,1,2], [3,4,5], [6,7,8],
+      [0,3,6],[1,4,7],[2,5,8],
+      [0,4,8],[2,4,6]
+    ]
+
+    winningCondition.forEach(array =>{
+      let circleWins = array.every(cell => cells[cell] === "circle")
+      if(circleWins){
+        setWinningMessage("Circle Wins!")
+        return
+      }
+    })
+
+    winningCondition.forEach(array =>{
+      let crossWins = array.every(cell => cells[cell] === "cross")
+      if(crossWins){
+        setWinningMessage("Cross Wins!")
+        return
+      }
+    })
+  }
+
+  useEffect(()=>
+  {
+    checkScore()
+    console.log("checking score")
+  },[cells])
+
   return (
     <div className="App">
       <div className="gameboard">
@@ -17,6 +48,9 @@ const App = () => {
             cell={cell}
             go={go}
             setGo={setGo}
+            cells={cells}
+            setCells={setCells}
+            winningMessage={winningMessage}
          />
          )}
       </div>
